9f5d7300980b85ca8b068149d5f08e5f4883ce98
hyunseoki <hyunseoki@kaist.ac.kr>
2022-06-30 16:46:46 +0900

    update

--- scripts/cuda0.sh
+++ scripts/cuda0.sh
@@ -6,15 +6,14 @@ COMMENTS = "cos_scheduler_T25_w_L1Loss"
 BASE_PATH = "./data/resized_scale5"
 SAVE_FOLDER = "./checkpoint/$COMMENTS""
 
-python main.py
-            --epochs 600 \
-            --batch_size 16 \
-            --lr 1e-3 \
-            --T0 25 \
-            --device $DEVICE \
-            --model $MODEL \
-            --num_kp 36 \
-            --base_path $BASE_PATH \
-            --save_folder $SAVE_FOLDER \
-            --comments $COMMENTS
+python main.py --epochs 600 \
+                --batch_size 16 \
+                --lr 1e-3 \
+                --T0 25 \
+                --device $DEVICE \
+                --model $MODEL \
+                --num_kp 36 \
+                --base_path $BASE_PATH \
+                --save_folder $SAVE_FOLDER \
+                --comments $COMMENTS
 
