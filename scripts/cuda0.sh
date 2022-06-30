6a7d949bb55162586cfa7697d432625e426fdeb0
hyunseoki <hyunseoki@kaist.ac.kr>
2022-06-30 16:43:19 +0900

    update

--- scripts/cuda0.sh
+++ scripts/cuda0.sh
@@ -1,20 +1,20 @@
-$device = "cuda:0"
+$DEVICE = "cuda:0"
 
 ###############################################################################################
-$model = "smp"
-$comments = "cos_scheduler_T25_w_L1Loss"
-$base_path = "./data/resized_scale5"
-$save_folder = "./checkpoint/$comments"
+# $model = "smp"
+# $comments = "cos_scheduler_T25_w_L1Loss"
+# $base_path = "./data/resized_scale5"
+# $save_folder = "./checkpoint/$comments"
 
-python main.py \
-            --epochs 600 \
-            --batch_size 16 \
-            --lr 1e-3 \
-            --T0 25 \
-            --device $device \
-            --model $model \
-            --num_kp 36 \
-            --base_path $base_path \
-            --save_folder $save_folder \
-            --comments $comments
+python main.py
+            # --epochs 600 \
+            # --batch_size 16 \
+            # --lr 1e-3 \
+            # --T0 25 \
+            --device $DEVICE \
+            # --model $model \
+            # --num_kp 36 \
+            # --base_path $base_path \
+            # --save_folder $save_folder \
+            # --comments $comments
 
